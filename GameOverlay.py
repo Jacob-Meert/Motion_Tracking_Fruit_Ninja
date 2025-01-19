5b4ae53a36ffbc3fa21ae5a5552e27696cf22d72
Jacob-Meert <jacobameert@gmail.com>
2025-01-19 16:54:29 -0500

    intialize GameOverlay

---
+++ GameOverlay.py
@@ -0,0 +1,10 @@
+import pygame
+import numpy
+import random
+import time
+
+pygame.init()
+window_width = 640
+window_height = 480
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Motion-Box")
