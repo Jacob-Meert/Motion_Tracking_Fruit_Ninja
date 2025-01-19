ff83b10af89075f376035a6231cf6c6f8b8b9898
Jacob-Meert <jacobameert@gmail.com>
2025-01-19 17:26:08 -0500

    Game overlay

--- GameOverlay.py
+++ GameOverlay.py
@@ -1,10 +1,27 @@
 import pygame
+import cv2
 import numpy
 import random
 import time
 
-pygame.init()
-window_width = 640
-window_height = 480
-screen = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Motion-Box")
+def launchGame():
+    #initialize game and window
+    pygame.init()
+    window_width = 640
+    window_height = 480
+    screen = pygame.display.set_mode((window_width, window_height))
+    pygame.display.set_caption("Motion-Box")
+
+    background_color = (0,0,255)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            screen.fill(background_color) 
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+launchGame()
