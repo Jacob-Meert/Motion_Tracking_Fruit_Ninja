d03667c51d4ff342c048d530636e926d4d48f8e7
Arshawn Vossoughi <arshawnv@gmail.com>
2025-01-20 15:01:19 -0500

    Game Overlay with Camera

--- GameOverlay.py
+++ GameOverlay.py
@@ -3,7 +3,6 @@ import cv2
 import numpy
 import random
 import time
-from MotionDetection.BodyCapture import get_frame
 import threading 
 
 def launchGame():
@@ -14,7 +13,12 @@ def launchGame():
     screen = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Motion-Box")
 
-    background_color = (0,0,255)
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Unable to access the camera")
+        return
+    
 
     running = True
 
@@ -23,17 +27,34 @@ def launchGame():
             if event.type == pygame.QUIT:
                 running = False
 
+        
             # Check for KEYDOWN event inside the event loop
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     print("Space Pressed!")
                     running = False  
 
-        # Fill the screen with the background color
-        screen.fill(background_color)
+        #capture frame
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Unable to read from the camera")
+            break
+
+        #Converts frame from BGR to RGB because pygame uses RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        frame = numpy.transpose(frame, (1, 0, 2))
+        frame = cv2.flip(frame, 0)  # Flip horizontally
+        #Convert the frame to a pygame surface
+        frame_surface = pygame.surfarray.make_surface(frame)
+
+        frame_surface = pygame.transform.scale(frame_surface,(window_width,window_height))
+        
+        screen.blit(frame_surface,(0,0))
 
         # Update the display
         pygame.display.flip()
+    cap.release()
     pygame.quit()
 
 launchGame()
